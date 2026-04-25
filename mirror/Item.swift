f90682e0aa700a630deaa57451f51b69e12016ee
Lokesh Pudari <pudari.lokesh@icloud.com>
2026-04-25 18:25:03 +0530

    Initial Commit

---
+++ mirror/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  mirror
+//
+//  Created by Lokesh Pudhari on 25/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ mirrorTests/mirrorTests.swift
@@ -0,0 +1,19 @@
+//
+//  mirrorTests.swift
+//  mirrorTests
+//
+//  Created by Lokesh Pudhari on 25/04/26.
+//
+
+import Testing
+@testable import mirror
+
+struct mirrorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
